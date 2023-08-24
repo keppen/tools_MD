76e2c23d1719fbcbe4207506e14465d460c6cca9
keppen <max.sza122@gmail.com>
2023-08-24 13:34:30 +0200

    update

--- main.py
+++ main.py
@@ -16,58 +16,33 @@ cluster = None
 start_time = time.time()
 
 
-def get_log(log_file, **kargs):
-    with open(log_file, 'r') as file:
-        print("{:<8}{:<8}{:<8}{:<8}{:<8}{:<8}\
-        {:<8}{:<8}{:<8}{:<8}{:<8}".format("Data id",
-                                          "Structure",
-                                          "Cluster",
-                                          "Truncate",
-                                          "Step",
-                                          "Plot type",
-                                          "plot name",
-                                          "Plot labels",
-                                          "Plot limits",
-                                          "Plot res",
-                                          "Data npz")
-              )
-        logs = file.readlines()
-        for log in logs:
-            log = log.split(';')
-            msg = ""
-            msg += f"{log[0]:<8}"
-            msg += f"{log[1]:<8}"
-            msg += f"{log[2]:<8}"
-            msg += f"{log[3]:<8}"
-            msg += f"{log[4]:<8}"
-            msg += f"{log[5]:<8}"
-            msg += f"{log[6]:<8}"
-            msg += f"{log[7]:<8}"
-            msg += f"{log[8]:<8}"
-            msg += f"{log[9]:<8}"
-            msg += f"{log[10]:<8}"
-            msg += f"{log[11]:<8}"
-            print(msg)
-
-
 def main(*arguments):
     """
     Docstring
     """
     option_actions = {
-        "-h": lambda: print(Visualize.__doc__) or exit(0),
-        "--help": lambda: print(Visualize.__doc__) or exit(0),
+        "-h": lambda: print(main.__doc__) or exit(0),
+        "--help": lambda: print(main.__doc__) or exit(0),
         "readlog": lambda: read_log(arguments),
-        "plot": lambda: run(arguments)
+        "run": lambda: run(arguments),
+        "rerun": lambda: rerun(arguments)
     }
 
-    for arg in arguments:
-        print(arg)
-        if arg in option_actions:
-            option_actions[arg]()
+    if arguments[1] in option_actions:
+        option_actions[arguments[1]]()
 
 
 def read_log(arguments):
+
+    structure = None
+    cluster = None
+    plot_name = None
+    plot_labels = None
+    plot_limits = None
+    plot_resolution = None
+    plot_type = None
+    data_id = None
+
     for index, opt in enumerate(arguments):
         if opt in ["-l", "--log-file"]:
             log_file = arguments[index + 1]
@@ -89,23 +64,33 @@ def read_log(arguments):
             plot_type = arguments[index + 1]
         if opt in ["-id", "--data-id"]:
             data_id = arguments[index + 1]
+        if opt in ["-h", "--help"]:
+            print(read_log.__doc__)
+            exit(0)
 
-    get_log(
+    print_log(
         log_file,
-        structure=structure or None,
-        cluster=cluster or None,
-        plot_name=plot_name or None,
-        plot_labels=plot_labels or None,
-        plot_limits=plot_limits or None,
-        plot_resolution=plot_resolution or None,
-        plot_type=plot_type or None,
-        data_id=data_id or None,
+        structure=structure,
+        cluster=cluster,
+        plot_name=plot_name,
+        plot_labels=plot_labels,
+        plot_limits=plot_limits,
+        plot_resolution=plot_resolution,
+        plot_type=plot_type,
+        data_id=data_id,
     )
 
     exit(0)
 
 
 def run(arguments):
+
+    truncate = None
+    step = None
+    options = None
+    no_plot = None
+    to_csv = None
+    log_file = None
 
     for index, opt in enumerate(arguments):
         if opt in ["-t", "--truncate"]:
@@ -124,6 +109,9 @@ def run(arguments):
             cluster = arguments[index + 1]
         if opt in ["--no-csv"]:
             to_csv = False
+        if opt in ["-h", "--help"]:
+            print(Visualize.__doc__)
+            exit(0)
 
     R = Visualize(
         structure,
@@ -134,10 +122,61 @@ def run(arguments):
         no_plot=no_plot,
         to_csv=to_csv,
         log_file=log_file
-    )
+        )
     R._init_run()
 
     exit(0)
+
+
+def rerun(arguments):
+    for index, opt in enumerate(arguments):
+        if opt in ["-l", "--log-file"]:
+            log_file = arguments[index + 1]
+        if opt in ["-id", "--data-id"]:
+            data_id = arguments[index + 1]
+        if opt in ["-h", "--help"]:
+            print(rerun.__doc__)
+            exit(0)
+
+    logs = open(log_file)
+    logs_lines = logs.readlines()
+    logs.close()
+
+    for entry in logs_lines:
+        fileds = entry.split(';')
+        if int(data_id) == str(fileds[0]):
+            run_arguments = fileds
+            break
+
+
+def print_log(log_file, **kargs):
+    with open(log_file, 'r') as file:
+        titles = [
+                "Data id",
+                "Structure",
+                "Cluster",
+                "Truncate",
+                "Step",
+                "Plot type",
+                "plot name",
+                "Plot labels",
+                "Plot limits",
+                "Plot res",
+                "Data npz"
+                    ]
+        widths = [8, 16, 25, 9, 9, 12, 18, 18, 25, 19, 18]
+
+        msg = [f"{t:<{s}}" for t, s in zip(titles, widths)]
+        print("".join(msg))
+        print("-" * sum(widths))
+
+        logs = file.readlines()
+        msg = ""
+        for log in logs:
+            fields = [field.strip() for field in log.split(';')]
+            formatted_fields = [f"{field:<{width}}" for field, width in zip(fields, widths)]
+            msg += "".join(formatted_fields) + '\n'
+        print(msg)
 
 
 if __name__ == "__main__":
